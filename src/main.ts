9d5a17e770a472089eb746b22c3aa4679346cadd
Joel RAZAFINJATOVO <joel.razafinjatovo@epitech.eu>
2022-04-27 21:51:10 +0200

    enable cors on front netlify url

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import {AppModule} from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
         cors: {
-            origin: "https://widgets-dashboard.netlify.app",
+            origin: "https://dashboard-widgets.netlify.app/",
             credentials: true
         }
     });
